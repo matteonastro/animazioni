2c76e0b541c1953223984f7400677a461a3447ef
matteonastro <nastro.matteo@iisgalvanimi.edu.it>
2021-12-02 12:38:14 +0000

    versione3

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
     },
     draw: function(component) {
         this.context.fillStyle = component.color;
         this.context.fillRect(component.x, component.y, component.width, component.height);
       }
-    
 }
+
+function updateGameArea() {
+  myGameArea.draw(redSquare);
+}
+
+
+
 var redSquare = {
     width: 20,
     height: 20,
@@ -25,18 +32,27 @@ var redSquare = {
     y: 120,
     color: "red"
   }
+  
   function up() {
     redSquare.y -= 30;
+   
   }
+
   function down() {
     redSquare.y += 30;
+   
   }
   function left() {
     redSquare.x -= 30;
+    
   }
   function right() {
     redSquare.x += 30;
+    
   }
+
+
+  
   
   
   
